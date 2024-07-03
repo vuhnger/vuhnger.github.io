7363ed3339d48dc3bd4ec455d9cacae7e1bb4cc6
vuhger <victor.uhnger@gmail.com>
2024-07-03 17:02:38 +0200

    change default route to /home

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
-import './App.css'
+import './App.css';
 import "./components/NavBar/NavBar.css";
-import { BrowserRouter as Router, Routes,  Route } from 'react-router-dom';
-import Home from './components/Home/Home.tsx'
-import About from './components/About/About.tsx'
-import Projects from './components/Projects/Projects.tsx'
-import NavBar from './components/NavBar/NavBar.tsx'
-import Dashboard from './components/Dashboard/Dashboard.tsx';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Home from './components/Home/Home';
+import About from './components/About/About';
+import Projects from './components/Projects/Projects';
+import NavBar from './components/NavBar/NavBar';
+import Dashboard from './components/Dashboard/Dashboard';
 
 export default function App() {
-
-  return(
+  return (
     <Router>
       <NavBar />
       <Routes>
-      
-        <Route index path="/home" element= {<Home/>} />
-          <Route path="/about" element={ <About/>} />
-          <Route path="/projects" element={ <Projects />} />
-          <Route path="/dashboard" element={<Dashboard/>} />
-      
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/" element={<Navigate to="/home" />} />
       </Routes>
     </Router>
   );
 }
-
