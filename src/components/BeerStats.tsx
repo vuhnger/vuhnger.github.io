83b2ba26e7b7be097b5ad7f64b9ba519a1109be4
vuhger <victor.uhnger@gmail.com>
2025-12-25 17:29:58 +0100

    fix(ui): remove unused import

--- src/components/BeerStats.tsx
+++ src/components/BeerStats.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Container, Row, Col, Card } from "react-bootstrap";
+import { Row, Col, Card } from "react-bootstrap";
 import { UntappdService, defaultUntappdData, mockUntappdData, UntappdUserData } from "../services/untappdService";
 
 const BeerStats: React.FC = () => {
